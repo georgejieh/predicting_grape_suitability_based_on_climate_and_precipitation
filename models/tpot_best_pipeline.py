fd04d33b4069ac3b4eeee4f40577589d5f164ff3
georgejieh <99384826+georgejieh@users.noreply.github.com>
2024-05-05 22:57:26 -0700

    Complete all model training. Start UI for delivery.

--- models/tpot_best_pipeline.py
+++ models/tpot_best_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.5105610200725882
-exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+# Average CV score on the training set was: 0.529524745399321
+exported_pipeline = ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.9500000000000001, min_samples_leaf=4, min_samples_split=4, n_estimators=100)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
